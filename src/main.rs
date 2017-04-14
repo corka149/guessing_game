b32ee0545ca4a1317867097fd69b10b1bedc0ab8
Corka <corka@rust-pc>
2017-04-14 11:48:56 +0200

    Intial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+
+extern crate rand; // We’re adding a extern crate rand; line to the top that lets Rust know we’ll be using that external dependency.
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,     // The _ is a catchall value
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+        }
+    }
+}
